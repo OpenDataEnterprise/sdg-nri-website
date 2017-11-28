b0f93036e177150442e3b98069f5565710020ec8
Andy Lo <andy@odenterprise.org>
2017-11-28 11:26:27 -0600

    Resource table facelift.

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,3 +1,123 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
   $('#directory-table').DataTable();
-});
+});*/
+
+(function () {
+  function loadJSON(filepath) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.overrideMimeType("application/json");
+      xhr.open('GET', filepath, true);
+      xhr.onload = function () {
+        if (xhr.status == "200") {
+  	      resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject(xhr.statusText);
+        }
+      }
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    });
+  };
+
+  function buildIndex(documents) {
+    return new Promise(function (resolve, reject) {
+      // Build Lunr index.
+      var idx = lunr(function () {
+        this.ref('id');
+        this.field('title');
+        this.field('organization');
+        this.field('date_published');
+        this.field('url');
+        this.field('type');
+
+        documents.forEach(function (doc, index) {
+          this.add(Object.assign({ 'id': index }, doc));
+        }, this);
+      });
+
+      resolve(idx);
+    });
+  }
+
+  var idx;
+  var resources;
+
+  // Load JSON data into Lunr index.
+  var filepath = '/search_data.json';
+  var documentsRequest = loadJSON(filepath);
+
+  documentsRequest.then(function (documents) {
+    resources = documents;
+    buildIndex(documents).then(function (result) {
+      idx = result;
+    });
+
+    Vue.component('directory-table', {
+      template: "#directory-table-template",
+      props: {
+        data: Array,
+        columns: Array,
+        filterKey: String
+      },
+      data: function () {
+        var sortOrders = {}
+        this.columns.forEach(function (key) {
+          sortOrders[key] = 1
+        })
+        return {
+          sortKey: '',
+          sortOrders: sortOrders
+        }
+      },
+      computed: {
+        filteredData: function () {
+          var sortKey = this.sortKey
+          var filterKey = this.filterKey && this.filterKey.toLowerCase()
+          var order = this.sortOrders[sortKey] || 1
+          var data = this.data
+          if (filterKey) {
+            data = data.filter(function (row) {
+              return Object.keys(row).some(function (key) {
+                return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+              })
+            })
+          }
+          if (sortKey) {
+            data = data.slice().sort(function (a, b) {
+              a = a[sortKey]
+              b = b[sortKey]
+              return (a === b ? 0 : a > b ? 1 : -1) * order
+            })
+          }
+          return data
+        }
+      },
+      filters: {
+        capitalize: function (str) {
+          return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+      },
+      methods: {
+        sortBy: function (key) {
+          this.sortKey = key;
+          this.sortOrders[key] = this.sortOrders[key] * -1;
+        },
+      },
+      delimiters: ['((', '))']
+    });
+
+    var search_app = new Vue({
+      el: '#directory',
+      data: {
+        searchQuery: '',
+        gridColumns: [
+          'title',
+          'organization',
+          'url'
+        ], 
+        resources: resources
+      }
+    });
+  });
+})();
