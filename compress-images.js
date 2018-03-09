786e0988aca83a593dd7665421b66d83de4f3aae
Andy Lo <andy@odenterprise.org>
2018-03-09 08:58:06 -0600

    Updated banner background.

--- compress-images.js
+++ compress-images.js
@@ -1,10 +1,10 @@
-var compress_images = require('compress-images'), INPUT_path_to_your_images, OUTPUT_path;
+const compress_images = require('compress-images');
 
-INPUT_path_to_your_images = '_app/images/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
-OUTPUT_path = 'assets/img/';
+const input_path = '_app/images/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+const output_path = 'assets/img/';
 
-compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: true}, false,
-  {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+compress_images(input_path, output_path, {compress_force: true, statistic: true, autoupdate: true}, false,
+  {jpg: {engine: 'mozjpeg', command: ['-quality', '90']}},
   {png: {engine: 'pngquant', command: ['--quality=20-50']}},
   {svg: {engine: 'svgo', command: '--multipass'}},
   {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(){
